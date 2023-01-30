d59e4828983fa50e017f8f770636f77ee3e7ae79
kyeiiih <kimanthikevin76@gmail.com>
2023-01-31 01:10:52 +0300

    lists with kotlin

---
+++ src/main/java/com/dsa/dsa/dsa2023/kotlindsa/WorkingWithLists.kt
@@ -0,0 +1,12 @@
+package com.dsa.dsa.dsa2023.kotlindsa
+
+class WorkingWithLists {
+    companion object {
+        val users = listOf(
+            User("Sam", 29),
+            User("Elijah", 25)
+        )
+
+
+    }
+}
